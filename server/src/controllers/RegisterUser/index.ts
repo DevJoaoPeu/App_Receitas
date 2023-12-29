77fb12228b0500a903be7f9489b40f2c5d684600
DevJoaoPeu <joaocanaldroid@gmail.com>
2023-12-29 09:51:02 -0300

    feat: buscando informações no body & verificação

---
+++ server/src/controllers/RegisterUser/index.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { prisma } from "../../Services/prisma";
+
+interface InforUser {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export const RegisterUser = async (req: Request, res: Response) => {
+  const { name, email, password }: InforUser = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({
+      error: true,
+      message: "Porfavor, preecha todos os campos!",
+    });
+  }
+};
