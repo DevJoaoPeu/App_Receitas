b583986fcb4150d22404a22779c2e7e2cda1ea40
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-01-02 10:06:16 -0300

    feat: rota de login

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { RouterDefault } from "./controllers/RotaTeste";
 import { RegisterUser } from "./controllers/RegisterUser";
+import { LoginUser } from "./controllers/LoginUser";
 
 const router = Router();
 
 router.get("/", RouterDefault)
 
 router.post("/register", RegisterUser)
+
+router.post("/login", LoginUser)
 
 export { router };
 
