fec1e0d75f5200c813b9d33d596766e674da3dc0
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-01-02 09:40:47 -0300

    feat: validação do email e password, e criação do token de login

--- server/src/controllers/LoginUser/index.ts
+++ server/src/controllers/LoginUser/index.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { prisma } from "../../Services/prisma";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 interface PropsUser {
   email: string;
@@ -12,16 +14,36 @@ export const LoginUser = async (req: Request, res: Response) => {
   if (!email || !password) {
     return res.status(400).json({
       error: true,
-      message: "Preencha todos os campos!",
+      message: "Preencha todos os campos corretamente!",
     });
   }
 
   const user = await prisma.user.findUnique({ where: { email } });
 
+  const passMatch = await compare(password, user?.password as string);
+
   if (!user) {
     return res.status(400).json({
       error: true,
-      message: "Usuario não encontrado",
+      message: "Usuario/senha incorretos",
     });
   }
+
+  if (!passMatch) {
+    return res.status(400).json({
+      error: true,
+      message: "Usuario/senha incorretos",
+    });
+  }
+
+  const token = sign(
+    {
+      name: user.name,
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+    { subject: user.id, expiresIn: "30d" }
+  );
+
+  
 };
