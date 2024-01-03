ec06c95eb305e15ce0e36b61d5194762d0accdba
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-01-03 10:31:08 -0300

    feat: recebendos os dados da requisição, e retornando a resposta do servidor

--- server/src/controllers/CreatedNewPost/index.ts
+++ server/src/controllers/CreatedNewPost/index.ts
@@ -1,19 +1,39 @@
 import { Response, Request } from "express";
+import { prisma } from "../../Services/prisma";
 
 interface Props {
   title: string;
   description: string;
   ingredient: string;
-  preparartion_mode: string;
+  preparation_mode: string;
   movie_link: string;
+  userId: string;
 }
 
-const CreatedNewPost = (req: Request, res: Response) => {
+ export const CreatedNewPost = async (req: Request, res: Response) => {
   const {
     title,
     description,
     ingredient,
     movie_link,
-    preparartion_mode,
+    preparation_mode,
+    userId
   }: Props = req.body;
+
+  const receitas = await prisma.receitas.create({
+    data: {
+        title,
+        description,
+        ingredient,
+        movie_link,
+        preparation_mode,
+        userId
+    },
+  });
+
+  return res.json({
+    error: false,
+    message: "Receita cadastrada com sucesso!",
+    receitas
+  })
 };
