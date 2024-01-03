83020cd4e6f3b984d855f83558527d1ac5263040
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-01-03 10:31:58 -0300

    feat: rota para criação de receitas

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,6 +3,7 @@ import { RouterDefault } from "./controllers/RotaTeste";
 import { RegisterUser } from "./controllers/RegisterUser";
 import { LoginUser } from "./controllers/LoginUser";
 import { isAuthenticated } from "./Middlewares";
+import { CreatedNewPost } from "./controllers/CreatedNewPost";
 
 const router = Router();
 
@@ -11,6 +12,8 @@ router.get("/", isAuthenticated ,RouterDefault)
 router.post("/register", RegisterUser)
 
 router.post("/login", LoginUser)
+
+router.post("/create", CreatedNewPost)
 
 export { router };
 
