3ca731be4985ffef99c235bcd5054d3e7ed0ebe1
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-01-04 00:44:40 -0300

    feat: buscando a lista e retornando os dados

--- server/src/controllers/ListPost/index.ts
+++ server/src/controllers/ListPost/index.ts
@@ -2,5 +2,18 @@ import { Response, Request } from "express";
 import { prisma } from "../../Services/prisma";
 
 export const ListPost = async (req: Request, res: Response) => {
-  
+   const infos = await prisma.receitas.findMany();
+
+   if(!infos){
+    return res.status(400).json({
+        error: true,
+        message: "Error ao buscar"
+    })
+   }
+
+   return res.status(200).json({
+    error: false,
+    message: "Informações carregadas com sucesso!",
+    infos
+   })
 };
