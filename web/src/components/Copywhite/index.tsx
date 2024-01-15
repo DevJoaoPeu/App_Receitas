348bec6fdfb5f35d1d8191881280a974a6cc3716
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-01-15 10:05:35 -0300

    feat: component do copywhite

---
+++ web/src/components/Copywhite/index.tsx
@@ -0,0 +1,7 @@
+export const Copywhite = () => {
+  return (
+    <div className="w-full text-center bg-blue-600 p-3 text-white fixed bottom-0 right-0">
+      <span>Todos os direitos reservados a João Pedro</span>
+    </div>
+  );
+};
