3c3678edbc7d73c50f47eeb47373b600b8139d0c
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-01-15 08:47:31 -0300

    feat: componente do input

---
+++ web/src/components/Input/index.tsx
@@ -0,0 +1,7 @@
+import { InputHTMLAttributes } from "react";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+export const Input = ({ ...rest }: InputProps) => {
+  return <input className="" {...rest} />;
+};
