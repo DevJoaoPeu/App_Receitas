9e8797b6d20c23426cfc20fc0ace348119c54647
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-01-15 10:02:13 -0300

    feat: estilos e finalização

--- web/src/components/Input/index.tsx
+++ web/src/components/Input/index.tsx
@@ -3,5 +3,5 @@ import { InputHTMLAttributes } from "react";
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 export const Input = ({ ...rest }: InputProps) => {
-  return <input className="" {...rest} />;
+  return <input className="bg-blue-500 px-3 py-2 text-white border-none outline-none placeholder-gray-300 rounded-md" {...rest} />;
 };
