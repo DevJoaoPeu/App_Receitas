7141977971e00942023d783c4454ef673d34323c
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-01-17 10:06:23 -0300

    feat: corrigindo href

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import logo from "../../public/logo.png";
+import logo from "../../public/logo.png"
 import { Input } from "@/components/Input";
 import { Button } from "@/components/Button";
 import { Copywhite } from "@/components/Copywhite";
