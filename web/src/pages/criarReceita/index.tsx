3ada1e78b66f1b206d04f60fba53cddbbacd77db
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-01-18 09:56:36 -0300

    feat: finalizando pagina de cadastrar receitas

--- web/src/pages/criarReceita/index.tsx
+++ web/src/pages/criarReceita/index.tsx
@@ -1,5 +1,31 @@
+import { Copywhite } from "@/components/Copywhite";
+import { Header } from "@/components/Header";
+import { Input } from "@/components/Input";
+import { Input2 } from "@/components/Input2";
+import { PiBowlFood } from "react-icons/pi";
+
 const CriarReceita = () => {
-    return <h1>Hello world</h1>
+  return (
+    <div>
+      <Header />
+      <div className="px-[250px] mt-[280px]">
+        <div>
+          <h1 className="flex justify-center gap-4 items-center text-[2.5rem]">
+            Cadastre sua receita <PiBowlFood />
+          </h1>
+        </div>
+        <div className="flex flex-col gap-1 items-center mt-8">
+          <Input2 placeholder="Titulo" />
+          <Input2 placeholder="Descrição" />
+          <Input2 placeholder="Ingredientes" />
+          <Input2 placeholder="Modo de preparo" />
+          <Input2 placeholder="Link do video" />
+          <button className="rounded-md bg-blue-600 text-white w-full px-2 py-[.4rem] max-w-[22rem]">Cadastrar</button>
+        </div>
+      </div>
+      <Copywhite />
+    </div>
+  );
 };
 
-export default CriarReceita
+export default CriarReceita;
