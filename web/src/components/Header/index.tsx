6ae6bc4547f0f50e25f31d0bcebbe374b13ec760
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-01-24 11:54:01 -0300

    feat: retirando a borda  da estilização

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import { FaSignOutAlt } from "react-icons/fa";
 
 export const Header = () => {
   return (
-    <div className="border-b-2 w-full px-[250px] justify-between  items-center flex p-3 fixed top-0">
+    <div className="w-full px-[250px] justify-between  items-center flex p-3 fixed top-0">
       <Image width={100} src={logo} alt="logo" />
       <FaSignOutAlt size={30}/>
     </div>
