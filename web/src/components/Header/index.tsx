9a13764509cb5462e30801054b954f7cf777d524
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-01-26 10:13:57 -0300

    style: Adicionando classes de responsividade

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -4,9 +4,19 @@ import { FaSignOutAlt } from "react-icons/fa";
 
 export const Header = () => {
   return (
-    <div className="w-full px-[250px] justify-between  items-center flex p-3 fixed top-0">
-      <Image width={100} src={logo} alt="logo" />
-      <FaSignOutAlt size={30}/>
+    <div className="w-full flex justify-between items-center py-3 px-4 xl:px-[250px] fixed top-0 bg-white">
+      <div className="sm:w-1/2 md:w-auto flex items-center">
+        <Image
+          src={logo}
+          alt="logo"
+          width={80}
+          height={50}
+          className="object-contain"
+        />
+      </div>
+      <div>
+        <FaSignOutAlt size={30} />
+      </div>
     </div>
   );
 };
