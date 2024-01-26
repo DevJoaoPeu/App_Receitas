9cae089b0c2773880a1dd90c9e718946879f7bbd
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-01-26 11:28:27 -0300

    style: Adicionando classes de responsividade

--- web/src/components/Modal/index.tsx
+++ web/src/components/Modal/index.tsx
@@ -4,33 +4,41 @@ import logo from "../../../public/feijoada.png";
 import { FiX } from "react-icons/fi";
 
 interface PropsModal {
-  modal: boolean
-  setModal: (props: boolean) => void
+  modal: boolean;
+  setModal: (props: boolean) => void;
 }
 
-export const ModalOrder = ({modal, setModal}: PropsModal) => {
-
+export const ModalOrder = ({ modal, setModal }: PropsModal) => {
   const customStyle = {
     overlay: {
       backgroundColor: "rgba(0, 0, 0, 0.8)",
     },
     content: {
       top: "50%",
-      bottom: "auto",
       left: "50%",
       right: "auto",
-      padding: "30px",
+      bottom: "auto",
+      marginRight: "-50%",
       transform: "translate(-50%, -50%)",
       backgroundColor: "#fff",
+      padding: "40px",
+      maxWidth: "90%"
     },
   };
 
   return (
     <Modal isOpen={modal} style={customStyle}>
-      <button onClick={() => setModal(false)} className="flex float-end text-red-500 text-xl"><FiX /></button>
-      <div className="flex justify-between gap-10">
-        <div>
-          <Image src={logo} alt="logo" className="w-200" />
+      <button
+        onClick={() => setModal(false)}
+        className="absolute top-3 right-3 text-red-500 text-xl"
+      >
+        <FiX />
+      </button>
+      <div className="flex flex-col sm:flex-row gap-10 items-center">
+        <div className="sm:w-1/2 sm:flex sm:gap-9">
+          <div className="w-full sm:w-auto">
+            <Image src={logo} alt="logo" width={300} height={200} />
+          </div>
           <div className="px-5 py-5">
             <ul className="list-disc">
               <li>Feijão</li>
@@ -42,23 +50,23 @@ export const ModalOrder = ({modal, setModal}: PropsModal) => {
             </ul>
           </div>
         </div>
-        <div>
+        <div className="sm:w-1/2">
           <div className="text-center">
             <h1 className="text-xl font-bold mb-3">Strogonoff de frango</h1>
-            <h4 className="text-sm ">
-              Strogonoff de frango cremoso para toda a familia, com poucos
-              ingredientes e um otimo sabor!
+            <h4 className="text-sm sm:text-[17px]">
+              Strogonoff de frango cremoso para toda a família, com poucos
+              ingredientes e um ótimo sabor!
             </h4>
           </div>
-          <div className="mt-[50px]">
-            <p className="text-sm">
+          <div className="mt-5">
+            <p className="text-sm sm:text-[17px]">
               Modo de preparo: Lorem ipsum dolor sit amet, consectetur
               adipiscing elit, sed do eiusmod tempor incididunt ut labore et
               dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis
               ullamcorper velit sed. Sagittis eu volutpat odio facilisis mauris.
               Pharetra diam sit amet nisl suscipit adipiscing. Turpis in eu mi
               bibendum neque. Euismod elementum nisi quis eleifend quam
-              adipiscing vitae proin.{" "}
+              adipiscing vitae proin.
             </p>
           </div>
         </div>
