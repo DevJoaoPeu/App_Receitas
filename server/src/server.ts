0a92927143870ce016ac1f7990bbad27cf133e7d
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-02-01 11:48:18 -0300

    feat: adicionando cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,12 @@
 import Express from "express";
 import { router } from "./routes";
+import cors from "cors"
 
 const PORT = 3333
 const app = Express();
 
 app.use(Express.json())
+app.use(cors());
 app.use(router);
 
 app.listen(PORT, () => {
