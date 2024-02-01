3fc1be154fa7a7e8c20dc7ac34d8954843730df4
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-02-01 11:50:46 -0300

    feat: configurando rota /login e validando o token

--- web/src/context/AuthContext.tsx
+++ web/src/context/AuthContext.tsx
@@ -1,10 +1,72 @@
-import { ReactNode, createContext } from "react";
+import { api } from "@/services/apiClient";
+import Router from "next/router";
+import { destroyCookie, setCookie } from "nookies";
+import { ReactNode, createContext, useState } from "react";
+import { toast } from "react-toastify";
+
+type ContextData = {
+    signIn: (credentials: SignInProps) => Promise<void>
+}
 
 type AuthProviderProps = {
   children: ReactNode;
 };
 
-export const AuthContext = createContext({});
+interface SignInProps {
+  email: string;
+  password: string;
+}
+
+interface UserProps {
+  name: string;
+  id: string;
+  token: string;
+}
+
+export const AuthContext = createContext({} as ContextData);
+
+export const SignOut = () => {
+  try {
+    destroyCookie(undefined, "@appPedidos.token");
+    Router.push("/");
+  } catch (error) {
+    console.log("Error ao deslogar");
+  }
+};
+
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  const [user, setUser] = useState({} as UserProps);
+
+  const signIn = async ({ email, password }: SignInProps) => {
+    try {
+      const response = await api.post("/login", { email, password });
+
+      const { id, name, token } = response.data;
+      setCookie(undefined, "@appPedidos.token", token, {
+        maxAge: 60 * 60 * 24 * 30,
+        path: "/",
+      });
+
+      setUser({
+        id,
+        name,
+        token,
+      });
+
+      api.defaults.headers["Authorization"] = `Bearer ${token}`
+
+      toast.success("Logadoo com sucesso", {
+        position: "top-center"
+      });
+
+      Router.push("/home")
+    } catch (error) {
+        toast.error("Erro ao acessar", {
+            position: "top-center",
+          });
+          console.log("error ao acessar", error);
+    }
+  };
+
+  return <AuthContext.Provider value={{signIn}}>{children}</AuthContext.Provider>;
 };
