65ef197835e59d20d1c0773405e95f78e2c870b4
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-02-02 10:23:19 -0300

    feat: criando sistema de login

--- web/src/pages/signup/index.tsx
+++ web/src/pages/signup/index.tsx
@@ -3,17 +3,47 @@ import { Button } from "@/components/Button";
 import { Copywhite } from "@/components/Copywhite";
 import { Input } from "@/components/Input";
 import Image from "next/image";
+import { FormEvent, useState } from "react";
+import { toast } from "react-toastify";
 
 const Signup = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignUp = (e: FormEvent) => {
+    e.preventDefault();
+
+    if(!name || !email || !password){
+      toast.error("Preecha todos os campos", {
+        position: "top-center"
+      })
+    }
+
+    console.log(name, email, password)
+  };
+
   return (
     <div>
       <div className="flex flex-col items-center justify-center mt-[100px]">
         <Image src={logo} className="mb-8" width={170} alt="logo" />
         <div className="flex flex-col max-w-[470px] gap-1 w-full px-4">
-          <Input type="text" placeholder="Nome" />
-          <Input type="text" placeholder="Email" />
-          <Input type="password" placeholder="Senha" />
-          <Button>Login</Button>
+          <Input
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Nome"
+          />
+          <Input
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="Email"
+          />
+          <Input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Senha"
+          />
+          <Button onClick={handleSignUp}>Login</Button>
         </div>
         <span className="m-8">
           Já tem conta?{" "}
