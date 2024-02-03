b64b9e7caec0ec1d10c57e54d7130fc1a97313eb
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-02-03 00:12:53 -0300

    feat: chamando a função signOut, para deslogar o usuario

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -2,8 +2,13 @@ import Image from "next/image";
 import logo from "../../../public/logo.png";
 import { FaSignOutAlt } from "react-icons/fa";
 import Link from "next/link";
+import { useContext } from "react";
+import { AuthContext } from "@/context/AuthContext";
 
 export const Header = () => {
+
+  const { SignOut } = useContext(AuthContext)
+
   return (
     <div className="w-full flex justify-between items-center py-3 px-4 xl:px-[250px] fixed top-0 bg-white">
       <div className="sm:w-1/2 md:w-auto flex items-center">
@@ -17,7 +22,7 @@ export const Header = () => {
           />
         </Link>
       </div>
-      <div>
+      <div onClick={SignOut}>
         <FaSignOutAlt size={30} />
       </div>
     </div>
