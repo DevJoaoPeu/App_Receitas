27917251b9be600df36f9de85cf1ae44eeac7164
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-02-03 00:14:19 -0300

    feat: criando função signUp para cadastrar usuario

--- web/src/context/AuthContext.tsx
+++ web/src/context/AuthContext.tsx
@@ -5,8 +5,10 @@ import { ReactNode, createContext, useState } from "react";
 import { toast } from "react-toastify";
 
 type ContextData = {
-    signIn: (credentials: SignInProps) => Promise<void>
-}
+  signIn: (credentials: SignInProps) => Promise<void>;
+  signUp: (credentials: SignUpProps) => Promise<void>;
+  SignOut: () => void
+};
 
 type AuthProviderProps = {
   children: ReactNode;
@@ -23,6 +25,12 @@ interface UserProps {
   token: string;
 }
 
+interface SignUpProps {
+  name: string;
+  email: string;
+  password: string;
+}
+
 export const AuthContext = createContext({} as ContextData);
 
 export const SignOut = () => {
@@ -37,12 +45,28 @@ export const SignOut = () => {
 export const AuthProvider = ({ children }: AuthProviderProps) => {
   const [user, setUser] = useState({} as UserProps);
 
+  const signUp = async ({ name, email, password }: SignUpProps) => {
+    try {
+      const response = await api.post("/register", { name, email, password });
+
+      toast.success("Conta criada com sucesso", {
+        position: "top-center",
+      });
+
+      Router.push("/");
+    } catch (error) {
+      toast.error("Erro ao cadastrar", {
+        position: "top-center",
+      });
+    }
+  };
+
   const signIn = async ({ email, password }: SignInProps) => {
     try {
       const response = await api.post("/login", { email, password });
 
       const { id, name, token } = response.data.data;
-      console.log(response)
+      console.log(response);
       setCookie(undefined, "@appPedidos.token", token, {
         maxAge: 60 * 60 * 24 * 30,
         path: "/",
@@ -54,20 +78,24 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
         token,
       });
 
-      api.defaults.headers["Authorization"] = `Bearer ${token}`
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
 
       toast.success("Logadoo com sucesso", {
-        position: "top-center"
+        position: "top-center",
       });
 
-      Router.push("/home")
+      Router.push("/home");
     } catch (error) {
-        toast.error("Erro ao acessar", {
-            position: "top-center",
-          });
-          console.log("error ao acessar", error);
+      toast.error("Erro ao acessar", {
+        position: "top-center",
+      });
+      console.log("error ao acessar", error);
     }
   };
 
-  return <AuthContext.Provider value={{signIn}}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ signIn, signUp, SignOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
