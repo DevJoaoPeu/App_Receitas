6b874c9bc8fdf60658891dab036f663e3e619664
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-02-07 11:03:32 -0300

    feat: criando listagem pelo id, buscando no params

---
+++ server/src/controllers/ListPostId/index.ts
@@ -0,0 +1,22 @@
+import { Response, Request } from "express";
+import { prisma } from "../../Services/prisma";
+
+export const ListPostId = async (req: Request, res: Response) => {
+
+  const { id } = req.params
+
+  const infos = await prisma.receitas.findUnique({where: {id}});
+
+  if (!infos) {
+    return res.status(400).json({
+      error: true,
+      message: "Error ao buscar",
+    });
+  }
+
+  return res.status(200).json({
+    error: false,
+    message: "Informações carregadas com sucesso!",
+    infos,
+  });
+};
