4da0f89a7ad6cab10fa0d552d6a6eb8cff8865c8
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-02-07 00:44:59 -0300

    feat: dando um map na resposta do banco

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -4,18 +4,42 @@ import imageMain from "../../../public/feijoada.png";
 import { FaSearch } from "react-icons/fa";
 import { Copywhite } from "@/components/Copywhite";
 import { ModalOrder } from "@/components/Modal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
+import { api } from "@/services/apiClient";
+
+type PropsReceita = {
+  title: string;
+  description: string;
+  ingredients: string;
+  preparation: string;
+  movie: string;
+};
 
 const Home = () => {
   const [modal, setModal] = useState(false);
+
+  const [response, setResponse] = useState<PropsReceita[]>([]);
+
+  useEffect(() => {
+    const handleReponse = async () => {
+      const response = await api.get("/list");
+
+      const data: PropsReceita[] = response.data.infos;
+      setResponse(data);
+    };
+    handleReponse();
+    console.log(response);
+  }, []);
 
   return (
     <div>
       <Header />
       <div className="mt-[150px] sm:mt-[200px] flex flex-col-reverse pb-[3.5rem] sm:flex-row items-center sm:px-[250px] px-3 sm:justify-between">
         <div className="flex flex-col w-full sm:text-left">
-          <h1 className="text-[2rem] sm:text-[3rem] leading-[4rem]">Receitas do chefe</h1>
+          <h1 className="text-[2rem] sm:text-[3rem] leading-[4rem]">
+            Receitas do chefe
+          </h1>
           <h4 className="text-[1.3rem] text-gray-500">
             Receitas enviadas pelos visitantes
           </h4>
@@ -23,7 +47,10 @@ const Home = () => {
             <h4 className="text-gray-700 text-[1.5rem]">
               Cadastre a sua receita já
             </h4>
-            <Link href="/criarReceita" className="bg-blue-600 text-white px-[3rem] py-2 text-[1rem] rounded-md">
+            <Link
+              href="/criarReceita"
+              className="bg-blue-600 text-white px-[3rem] py-2 text-[1rem] rounded-md"
+            >
               Cadastre
             </Link>
           </div>
@@ -48,33 +75,20 @@ const Home = () => {
           </button>
         </div>
         <div className="flex flex-col gap-4 mb-10">
-          <div className="rounded-md flex items-center justify-between bg-blue-600 text-white px-4 max-w-[470px] w-full py-3 mx-auto">
-            <h4>Strogonoff de frango</h4>
-            <button
-              onClick={() => setModal(true)}
-              className="bg-blue-500 py-1 px-3 rounded-md"
+          {response.map((item, index) => (
+            <div
+              key={index}
+              className="rounded-md flex items-center justify-between bg-blue-600 text-white px-4 max-w-[470px] w-full py-3 mx-auto"
             >
-              Veja a receita
-            </button>
-          </div>
-          <div className="rounded-md flex items-center justify-between bg-blue-600 text-white px-4 py-3 max-w-[470px] w-full mx-auto">
-            <h4>Strogonoff de frango</h4>
-            <button
-              onClick={() => setModal(true)}
-              className="bg-blue-500 py-1 px-3 rounded-md"
-            >
-              Veja a receita
-            </button>
-          </div>
-          <div className="rounded-md flex items-center justify-between bg-blue-600 text-white px-4 py-3 max-w-[470px] w-full mx-auto">
-            <h4>Strogonoff de frango</h4>
-            <button
-              onClick={() => setModal(true)}
-              className="bg-blue-500 py-1 px-3 rounded-md"
-            >
-              Veja a receita
-            </button>
-          </div>
+              <h4>{item.title}</h4>
+              <button
+                onClick={() => setModal(true)}
+                className="bg-blue-500 py-1 px-3 rounded-md"
+              >
+                Veja a receita
+              </button>
+            </div>
+          ))}
         </div>
         <ModalOrder modal={modal} setModal={setModal} />
       </div>
