5a52875d67601a56537faf7bb29f8829f2c6e5bc
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-02-07 11:00:19 -0300

    feat: adicionando propriedade para o modal

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,7 +2,10 @@ import { AuthProvider } from "@/context/AuthContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
+import Modal from "react-modal"
 import "react-toastify/dist/ReactToastify.css";
+
+Modal.setAppElement('#__next'); 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
