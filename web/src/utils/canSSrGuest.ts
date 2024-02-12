1426dd8d562ecb5e6106a8e50ca14ccb0e51b50c
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-02-12 10:50:49 -0300

    feat: corrigindo tipagem

--- web/src/utils/canSSrGuest.ts
+++ web/src/utils/canSSrGuest.ts
@@ -5,20 +5,22 @@ import {
 } from "next";
 import { parseCookies } from "nookies";
 
-export function canSSRGuest<P>(fn: GetServerSideProps<P>) {
-    return async (
-        ctx: GetServerSidePropsContext
-    ): Promise<GetServerSidePropsResult<P>> =>{
-        const cookies = parseCookies(ctx);
+export function canSSRGuest<P extends { [key: string]: any }>(
+  fn: GetServerSideProps<P>
+) {
+  return async (
+    ctx: GetServerSidePropsContext
+  ): Promise<GetServerSidePropsResult<P>> => {
+    const cookies = parseCookies(ctx);
 
-        if(cookies["@appPedidos.token"]){
-            return{
-              redirect:{
-                destination: "/home",
-                permanent: false
-              }
-            }
-        }
-        return await fn(ctx)
+    if (cookies["@appPedidos.token"]) {
+      return {
+        redirect: {
+          destination: "/home",
+          permanent: false,
+        },
+      };
     }
+    return await fn(ctx);
+  };
 }
