5fcc512f11b79d23a3c36cbd898223f8f1936e6c
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-02-13 01:07:15 -0300

    feat: desativando o botão após clicado

--- web/src/components/Button/index.tsx
+++ web/src/components/Button/index.tsx
@@ -1,9 +1,25 @@
 import { ReactNode, ButtonHTMLAttributes } from "react";
+import { FaSpinner } from "react-icons/fa";
 
 interface PropsButton extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
+  loading: boolean;
 }
 
-export const Button = ({ children, ...rest }: PropsButton) => {
-  return <button className="bg-blue-600 py-2 rounded-md text-white" {...rest}>{children}</button>;
+export const Button = ({ children, loading, ...rest }: PropsButton) => {
+  return (
+    <button
+      className={` ${loading ? "bg-blue-800": "bg-blue-600"} py-2 rounded-md text-white`}
+      disabled={loading}
+      {...rest}
+    >
+      {loading ? (
+        <span className="flex justify-center py-1 ">
+          <FaSpinner color="var(--white)" />
+        </span>
+      ) : (
+        <a>{children}</a>
+      )}
+    </button>
+  );
 };
